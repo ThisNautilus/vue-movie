f4d931e9aecf2d5cad16a15288d190fe224b2c81
ThisNautilus <zziaocean@foxmail.com>
2019-08-10 21:41:35 +0800

    search finish

--- src/main.js
+++ src/main.js
@@ -6,6 +6,11 @@ import store from './stores' // 此写法是寻找stores目录下的index.js
 import axios from 'axios'
 Vue.prototype.axios = axios;
 
+// 定义全局过滤器用于处理图片的宽高
+Vue.filter('setWH', (url, arg) => {
+    return url.replace(/w\.h/, arg);
+});
+
 Vue.config.productionTip = false
 
 new Vue({
